4546be694def477af64f962d375482378210996b
sanjay123-py <sanjaysanjay1270@gmail.com>
2024-02-08 21:00:12 +0530

    cicd pred-1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8
-RUN mkdir consumer1
-COPY . ./consumer1/
-WORKDIR /consumer1
+RUN mkdir consumer
+COPY . ./consumer/
+WORKDIR /consumer
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 RUN apt update -y && apt install awscli -y
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,5 @@
 import os
 from pathlib import Path
-SCHEMA_FILE_PATH = '/consumer1/sample_data/movie_analytics_data/schema_structure.txt'
+SCHEMA_FILE_PATH = '/consumer/sample_data/movie_analytics_data/schema_structure.txt'
 # DATA_PATH = '/Users/sanjay/Downloads/RDS/sample_data/movie_analytics_data/ratings.dat'
 TOPIC = 'RDS_MOVIE_TOPIC'
